94116b3c97ab9238c7f37aa07916f258a3ae88fc
ankushKun <ankush4singh@gmail.com>
2023-07-10 11:44:57 +0530

    replace img with Image

--- src/pages/components/infiniteScroll.js
+++ src/pages/components/infiniteScroll.js
@@ -1,3 +1,11 @@
+import Image from "next/image"
+
+const data = [
+    "http://dummyimage.com/600x400/000000/ffffff.png",
+    "http://dummyimage.com/600x400/eeeeee/cccccc.png",
+    "http://dummyimage.com/600x400/000000/ffffff.png",
+    "http://dummyimage.com/600x400/eeeeee/cccccc.png",
+]
 
 export default function InfiniteScroll() {
     return <div className=" overflow-clip">
@@ -7,10 +15,13 @@ export default function InfiniteScroll() {
         <article>
             <div>
                 <ul>
-                    <li><img src="http://dummyimage.com/600x400/000000/ffffff.png" /></li>
-                    <li><img src="http://dummyimage.com/600x400/eeeeee/cccccc.png" /></li>
-                    <li><img src="http://dummyimage.com/600x400/000000/ffffff.png" /></li>
-                    <li><img src="http://dummyimage.com/600x400/eeeeee/cccccc.png" /></li>
+                    {
+                        data.map((item, index) => {
+                            return <li key={index}>
+                                <Image src={item} width={600} height={400} />
+                            </li>
+                        })
+                    }
                 </ul>
             </div>
             <div>

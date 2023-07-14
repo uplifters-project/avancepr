6990e17713eb23ae3a81c51b6cd915b391609d12
ankushKun <ankush4singh@gmail.com>
2023-07-14 17:19:01 +0530

    testimonial news apis

--- firebase.config.js
+++ firebase.config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyAmRc9DDzMmNh_YT5PmQd3X6A_mJZG7WXA",
-    authDomain: "personal-projects-69.firebaseapp.com",
-    databaseURL: "https://personal-projects-69-default-rtdb.firebaseio.com",
-    projectId: "personal-projects-69",
-    storageBucket: "personal-projects-69.appspot.com",
-    messagingSenderId: "800698599496",
-    appId: "1:800698599496:web:9e0f1f4cac1c0b1584307d"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DB_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 }
---
+++ src/pages/api/news/add.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set, get, push } from "firebase/database";
+import { firebaseConfig } from "../../../../firebase.config";
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export default function handler(req, res) {
+    const { image, name, username, pass } = req.body;
+    if (!image || !name || !username || !pass) {
+        res.status(200).json({ status: "error", message: "Please fill all the fields" });
+        return;
+    }
+    const newNews = {
+        image: image,
+        name: name
+    }
+
+    if (username === process.env.USERNAME && pass === process.env.PASSWORD) {
+        push(ref(database, 'news/'), newNews);
+        res.status(200).json({ status: "success", message: "News added successfully" });
+    } else {
+        res.status(200).json({ status: "error", message: "Incorrect password" });
+    }
+}

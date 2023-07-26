056812cbdcce229a8c546d7a8287c7cb92945497
Prathamesh Mutkure <pmutkure009@gmail.com>
2023-07-27 02:29:57 +0530

    Added APIs

---
+++ src/lib/apis.ts
@@ -0,0 +1,67 @@
+import axios from "axios";
+
+export const server = axios.create({
+  baseURL: "http://localhost:8000",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+const getTestimonials = async (): Promise<Testimonial[]> => {
+  try {
+    const res = await server.get("/testimonials");
+
+    return res.data as Testimonial[];
+  } catch (e) {
+    return [];
+  }
+};
+
+const getOurWork = async (): Promise<Work[]> => {
+  try {
+    const res = await server.get("/our_work");
+
+    return res.data as Work[];
+  } catch (e) {
+    return [];
+  }
+};
+
+const getOurClients = async (): Promise<Client[]> => {
+  try {
+    const res = await server.get("/our_client");
+
+    return res.data as Client[];
+  } catch (e) {
+    return [];
+  }
+};
+
+const getLatestNews = async (): Promise<News[]> => {
+  try {
+    const res = await server.get("/latest_news");
+
+    return res.data as News[];
+  } catch (e) {
+    return [];
+  }
+};
+
+const submitEnquiryForm = async (data: EnquiryFormType): Promise<boolean> => {
+  try {
+    const res = await server.post("/testimonials", data, {});
+
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+export {
+  getTestimonials,
+  getOurWork,
+  getOurClients,
+  getLatestNews,
+  submitEnquiryForm,
+};
---
+++ types/models.d.ts
@@ -0,0 +1,44 @@
+interface Testimonial {
+  id: number;
+  order: number;
+  name: string;
+  designation: string;
+  content: string;
+  image: string;
+  updated_at: string;
+  created_at: string;
+}
+
+interface Work {
+  id: number;
+  order: number;
+  content: string;
+  image: string;
+  updated_at: string;
+  created_at: string;
+}
+
+interface Client {
+  id: number;
+  order: number;
+  image: string;
+  updated_at: string;
+  created_at: string;
+}
+
+interface News {
+  id: number;
+  order: number;
+  content: string;
+  image: string;
+  updated_at: string;
+  created_at: string;
+}
+
+interface EnquiryFormType {
+  full_name: string;
+  email: string;
+  phone: string;
+  company_name: string;
+  enquiry: string;
+}

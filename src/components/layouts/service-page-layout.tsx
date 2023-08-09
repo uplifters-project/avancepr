dd70f1e13c9b771099b38c024efdb9203c010279
Prathamesh Mutkure <pmutkure009@gmail.com>
2023-08-09 13:35:44 +0530

    Modularise Service Pages

---
+++ src/components/layouts/service-page-layout.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import MainLayout from "./main-layout";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
+const ServicePageLaypout: React.FC<{
+  heading: string;
+  label: string;
+  title: string;
+  description: string;
+  image: string;
+  data: {
+    heading: string;
+    text: string;
+  }[];
+  children: React.ReactNode;
+  className?: string;
+}> = ({
+  heading,
+  label,
+  description,
+  image,
+  data,
+  title,
+  children,
+  className,
+}) => {
+  return (
+    <MainLayout className={cn("container pt-16 pb-24", className)}>
+      <div className="mb-12">
+        <h2 className="text-5xl text-center font-bold text-yellow-700 animate-showLetterByLetter">
+          {heading}
+        </h2>
+
+        <p className="text-lg text-center text-gray-600 mt-2">{label}</p>
+      </div>
+
+      <div className="flex flex-row gap-5 items-center">
+        <div className="w-2/3 pr-8">
+          <h1 className="font-heading text-4xl mb-3">{title}</h1>
+
+          <p>{description}</p>
+        </div>
+
+        <div className="w-1/3">
+          <Image src={image} width={612} height={408} alt="Content Marketing" />
+        </div>
+      </div>
+
+      <div>
+        <ul className="max-w-xl space-y-1 text-gray-500 list-inside dark:text-gray-400">
+          {data.map((item) => {
+            return (
+              <li className="flex flex-col">
+                <div className="flex justify-start items-center mb-2">
+                  <svg
+                    className="w-5 h-5 mr-2 text-green-500 dark:text-green-400 flex-shrink-0"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                  </svg>
+
+                  <h3 className="font-heading text-2xl font-bold">
+                    {item.heading}
+                  </h3>
+                </div>
+
+                <p className="mb-6">{item.text}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </MainLayout>
+  );
+};
+
+export default ServicePageLaypout;
---
+++ src/pages/services/content-marketing.tsx
@@ -0,0 +1,50 @@
+import { NextPage } from "next";
+
+import ServicePageLaypout from "@/components/layouts/service-page-layout";
+import Image from "next/image";
+
+const data = [
+  {
+    heading: "Social Media Content",
+    text: "We create social media content that is engaging and informative. Our team of experts ensures that the content is tailored to the specific needs of your target audience",
+  },
+  {
+    heading: "Website Content",
+    text: "We create website content that is optimized for search engines and is designed to attract and retain visitors. Our content is informative, engaging, and helps to establish your brand as an authority in your industry",
+  },
+  {
+    heading: "Articles",
+    text: "We create articles that are informative and engaging. Our team of writers has experience in a wide range of industries and can create articles that are tailored to your specific needs",
+  },
+  {
+    heading: "Blogs",
+    text: "We at Avance PR create Blogs that are informative, engaging, and designed to attract and retain readers. Our team of writers has experience in a wide range of industries and can create blogs that are tailored to your specific needs",
+  },
+];
+
+const description = `At Avance PR, our content marketing strategy revolves around the
+creation of high-quality and value-driven content that resonates
+with the intended audience. Through meticulous research and
+analysis, we identify the informational needs, preferences, and pain
+points of the target market. Leveraging these insights, our team of
+skilled content creators and strategists develop compelling and
+informative content that not only addresses the audience's needs but
+also showcases our clients' industry knowledge, thought leadership,
+and unique value propositions.`;
+
+const ContentMarketingPage: NextPage<{}> = ({}) => {
+  return (
+    <ServicePageLaypout
+      heading="Content Marketing"
+      label=""
+      image="/content_marketing.jpg"
+      title="What is Content Marketing"
+      data={data}
+      description={description}
+    >
+      <></>
+    </ServicePageLaypout>
+  );
+};
+
+export default ContentMarketingPage;

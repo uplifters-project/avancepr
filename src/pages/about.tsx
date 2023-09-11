c48def18e033150c26fbe67fd16acf6ccc8fe6c8
Prathamesh Mutkure <pmutkure009@gmail.com>
2023-09-11 15:52:19 +0530

    About Us Content Changes and Fixes

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -6,51 +6,79 @@ import Image from "next/image";
 const AboutPage: NextPage<{}> = ({}) => {
   return (
     <PageLaypout heading="About Us" label="" className="text-center">
-      <div className="max-w-5xl mx-auto">
-        <p>
-          AvancePR is a highly regarded and comprehensive PR agency
-          headquartered in Gurugram. Our core expertise lies in cultivating
-          impactful connections with influencers and crafting effective
-          corporate communications strategies. We take pride in offering a
-          diverse range of campaigns tailored to suit the unique needs of our
-          esteemed clientele. Through strategic media engagement, we diligently
-          work towards enhancing brand personality and reputation, bolstering
-          our clients' market presence.
-        </p>
-        <br />
-        <p>
-          At AvancePR, we specialize in several key sectors, including
-          Technology (B2B, B2C), Education, Consumer Technology, Infrastructure,
-          Retail, Lifestyle, Cybersecurity, FMCG, Real Estate, D2C, B2B2C,
-          Startups, Travel, Astro Tech, Hospitality, Logistics, Healthcare, and
-          Pharma. Our in-depth industry knowledge and extensive experience
-          enable us to provide targeted and impactful PR solutions within these
-          sectors. We understand the distinct challenges and opportunities that
-          each industry presents, allowing us to devise tailored strategies that
-          effectively meet our clients' specific objectives.
-        </p>
-        <br />
-        <p>
-          With a team of dedicated professionals, we are committed to adding
-          significant brand value for our clients, aligned with their
-          overreaching business goals. Our skilled PR experts bring a wealth of
-          knowledge and expertise to the table, implementing innovative and
-          results-driven approaches that drive tangible outcomes. By leveraging
-          our extensive network of media contacts and influencers, we secure
-          valuable exposure and create compelling narratives that resonate with
-          the target audience.
-        </p>
-        <br />
-        <p>
-          At AvancePR, our client-centric approach ensures that we fully
-          understand the unique needs and aspirations of each brand we work
-          with. We collaborate closely with our clients, developing strategic PR
-          campaigns that align with their vision and aspirations. By
-          meticulously crafting compelling messages and leveraging a mix of
-          traditional and digital PR channels, we strive to elevate brand
-          awareness, foster positive perceptions, and facilitate meaningful
-          connections between our clients and their target market.
-        </p>
+      <div className="max-w-6xl mx-auto flex flex-col gap-8">
+        <div className="grid gap-x-8 items-center grid-cols-5">
+          <div className="col-span-3 flex flex-col gap-4">
+            <p className="text-left">
+              AvancePR is a highly regarded and comprehensive PR agency
+              headquartered in Gurugram. Our core expertise lies in cultivating
+              impactful connections with influencers and crafting effective
+              corporate communications strategies. We take pride in offering a
+              diverse range of campaigns tailored to suit the unique needs of
+              our esteemed clientele. Through strategic media engagement, we
+              diligently work towards enhancing brand personality and
+              reputation, bolstering our clients' market presence.
+            </p>
+
+            <p className="text-left">
+              At AvancePR, we specialize in several key sectors, including
+              Technology (B2B, B2C), Education, Consumer Technology,
+              Infrastructure, Retail, Lifestyle, Cybersecurity, FMCG, Real
+              Estate, D2C, B2B2C, Startups, Travel, Astro Tech, Hospitality,
+              Logistics, Healthcare, and Pharma. Our in-depth industry knowledge
+              and extensive experience enable us to provide targeted and
+              impactful PR solutions within these sectors. We understand the
+              distinct challenges and opportunities that each industry presents,
+              allowing us to devise tailored strategies that effectively meet
+              our clients' specific objectives.
+            </p>
+          </div>
+
+          <Image
+            src="/images/about_us_1.jpeg"
+            height={426}
+            width={640}
+            alt=""
+            className="col-span-2"
+          />
+        </div>
+
+        <Separator className="max-w-5xl mx-auto my-8" />
+
+        <div className="grid gap-x-8 items-center grid-cols-5">
+          <Image
+            src="/images/about_us_2.jpeg"
+            height={480}
+            width={640}
+            alt=""
+            className="col-span-2"
+          />
+
+          <div className="col-span-3 flex flex-col gap-4">
+            <p className="text-left">
+              With a team of dedicated professionals, we are committed to adding
+              significant brand value for our clients, aligned with their
+              overreaching business goals. Our skilled PR experts bring a wealth
+              of knowledge and expertise to the table, implementing innovative
+              and results-driven approaches that drive tangible outcomes. By
+              leveraging our extensive network of media contacts and
+              influencers, we secure valuable exposure and create compelling
+              narratives that resonate with the target audience.
+            </p>
+
+            <p className="text-left">
+              At AvancePR, our client-centric approach ensures that we fully
+              understand the unique needs and aspirations of each brand we work
+              with. We collaborate closely with our clients, developing
+              strategic PR campaigns that align with their vision and
+              aspirations. By meticulously crafting compelling messages and
+              leveraging a mix of traditional and digital PR channels, we strive
+              to elevate brand awareness, foster positive perceptions, and
+              facilitate meaningful connections between our clients and their
+              target market.
+            </p>
+          </div>
+        </div>
       </div>
 
       <Separator className="max-w-4xl mx-auto my-16" />

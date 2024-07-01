e0774d68414d1dfc649761a35c0957c88895bc69
ankushKun <ankush4singh@gmail.com>
2024-07-01 20:52:23 +0530

    gtag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en" suppressHydrationWarning>
-      <Head />
+      <Head>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=AW-16466088790"/>
+        <Script>
+          {`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-16466088790');` }
+        </Script>
+      </Head>
       <body className="min-h-screen bg-background font-sans antialiased">
         <Main />
         <NextScript />
--- src/pages/thank-you.tsx
+++ src/pages/thank-you.tsx
@@ -1,7 +1,16 @@
 import MainLayout from "@/components/layouts/main-layout";
+import Script from "next/script";
 
 export default function ThankYou() {
     return <MainLayout>
+        <head>
+            <Script src="https://www.googletagmanager.com/gtag/js?id=AW-16466088790" />
+            <Script>
+                {`if(typeof window != 'undefined'){
+                    gtag('event', 'conversion', {'send_to': 'AW-16466088790/wws4CNuJwr0ZENam0qs9'});
+                }`}
+            </Script>
+        </head>
         <div className="my-20">
         <div className="text-center font-medium md:text-3xl my-5">
                 We would love to connect with you soon

ef7d1bf1695dcd722b107a423995bfc0221e8f5f
umangsangal <umangpradeepsangal@gmail.com>
2024-07-09 11:06:51 +0530

    Update thank-you.tsx commenting code

--- src/pages/thank-you.tsx
+++ src/pages/thank-you.tsx
@@ -4,14 +4,14 @@ import Script from "next/script";
 export default function ThankYou() {
     return <MainLayout>
         <head>
-         <script async src="https://www.googletagmanager.com/gtag/js?id=G-KLF4N81TNH"></script>
+{/*          <script async src="https://www.googletagmanager.com/gtag/js?id=G-KLF4N81TNH"></script>
          <script>
           window.dataLayer = window.dataLayer || [];
           function gtag(){window.dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', 'G-KLF4N81TNH');
         </script>
-        <script> gtag('event', 'conversion', {'send_to': 'AW-16466088790/wws4CNuJwr0ZENam0qs9'}); </script>
+        <script> gtag('event', 'conversion', {'send_to': 'AW-16466088790/wws4CNuJwr0ZENam0qs9'}); </script> */}
         </head>
         <div className="my-20">
         <div className="text-center font-medium md:text-3xl my-5">

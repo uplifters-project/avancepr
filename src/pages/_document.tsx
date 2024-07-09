a3711e2cb9a98761ef9c6dd9ac924cf91edf6668
umangsangal <umangpradeepsangal@gmail.com>
2024-07-09 12:53:20 +0530

    Update _document.tsx by umang final

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,13 +5,15 @@ export default function Document() {
   return (
     <Html lang="en" suppressHydrationWarning>
       <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KLF4N81TNH"></script>
-        <script>
-          { window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-KLF4N81TNH'); }
-        </script>
+         <Script src="https://www.googletagmanager.com/gtag/js?id=G-KLF4N81TNH"/>
+        <Script>
+          {`window.dataLayer = window.dataLayer || []; 
+          function gtag(){dataLayer.push(arguments);} 
+          gtag('js', new Date()); 
+          
+          gtag('config', 'G-KLF4N81TNH');
+          `}
+        </Script>
       </Head>
       <body className="min-h-screen bg-background font-sans antialiased">
         <Main />

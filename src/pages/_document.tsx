c4ab80a30f47d43d7174bacdd4b206b562ba9f80
umangsangal <umangpradeepsangal@gmail.com>
2024-07-09 14:23:42 +0530

    Update _document.tsx gtags final

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,8 @@ export default function Document() {
   return (
     <Html lang="en" suppressHydrationWarning>
       <Head>
-         <Script src="https://www.googletagmanager.com/gtag/js?id=G-KLF4N81TNH"/>
-        <Script>
+         <Script async src="https://www.googletagmanager.com/gtag/js?id=G-KLF4N81TNH" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
           {`window.dataLayer = window.dataLayer || []; 
           function gtag(){dataLayer.push(arguments);} 
           gtag('js', new Date()); 
